bf94924391c288e986afe94e78c9d1c2cdc3d559
Isabella Ramos Carvalho <au.isabella.r.c@hotmail.com>
2023-12-02 19:07:11 -0300

    feat: adicionado o perfil

---
+++ cupcakelivery/src/app/client/catalog/catalog.page.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-catalog',
+  templateUrl: './catalog.page.html',
+  styleUrls: ['./catalog.page.scss'],
+})
+export class CatalogPage implements OnInit {
+itensMock = [
+  {
+    id: 1,
+    product: "Cupcake de Morango",
+    price: 15.50
+  },
+  {
+    id: 2,
+    product: "Cupcake de Pistache",
+    price: 18
+  },
+  {
+    id: 3,
+    product: "Cupcake de Nutella com Ninho",
+    price: 17
+  },
+  {
+    id: 4,
+    product: "Cupcake de Baunilha",
+    price: 12.50
+  }
+]
+  constructor(
+    private location: Location
+  ) {}
+
+  ngOnInit() {}
+
+  addToKart() {
+    console.log("Adicionado")
+  }
+
+  formatPrice(price: number) {
+    const formattedPrice = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(price);
+
+    return formattedPrice;
+  }
+}
